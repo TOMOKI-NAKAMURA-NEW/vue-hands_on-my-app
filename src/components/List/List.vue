8635a28b38362554838bb7b34f8a50cd1ff3f7b5
TOMOKI-NAKAMURA-NEW <tomoki.nakamura.new@gmail.com>
2020-03-30 12:15:32 +0900

    List.vue　レイアウト修正

--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -1,74 +1,20 @@
 <template>
   <v-container grid-list-md>
     <v-layout row wrap>
-      <v-flex xs6 sm4 md3 lg2>
-        <v-card v-if="dataUrl">
-          <v-img :src="dataUrl"></v-img>
+      <v-flex xs6 sm4 md3 lg3>
+        <v-card v-if="dataImg">
+          <v-img :src="dataImg"></v-img>
           <v-card-actions>
+            <v-card-title>タイトル：{{ dataTitle }}</v-card-title>
             <v-spacer></v-spacer>
-            <v-btn icon>
+            <v-btn icon @click="deleteImg">
               <i class="far fa-trash-alt"></i>
             </v-btn>
           </v-card-actions>
         </v-card>
-      </v-flex>
-      <v-flex xs6 sm4 md3 lg2>
-        <v-card v-if="dataUrl">
-          <v-img :src="dataUrl"></v-img>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <i class="far fa-trash-alt"></i>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 sm4 md3 lg2>
-        <v-card v-if="dataUrl">
-          <v-img :src="dataUrl"></v-img>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <i class="far fa-trash-alt"></i>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 sm4 md3 lg2>
-        <v-card v-if="dataUrl">
-          <v-img :src="dataUrl"></v-img>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <i class="far fa-trash-alt"></i>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 sm4 md3 lg2>
-        <v-card v-if="dataUrl">
-          <v-img :src="dataUrl"></v-img>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <i class="far fa-trash-alt"></i>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs6 sm4 md3 lg2>
-        <v-card v-if="dataUrl">
-          <v-img :src="dataUrl"></v-img>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <i class="far fa-trash-alt"></i>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+        <v-btn x-large color="success" dark @click="getBooksImg">本画像を取得</v-btn>
       </v-flex>
     </v-layout>
-    <v-btn x-large color="success" dark @click="getBooksImg">本画像を取得</v-btn>
   </v-container>
 </template> 
 
@@ -76,17 +22,22 @@
 export default {
   data() {
     return {
-      dataUrl: ''
+      dataImg: '',
+      dataTitle: ''
     }
   },
   methods: {
-    async getBooksImg() {
+    getBooksImg() {
       let self = this
-      await this.$axios.get('https://api.openbd.jp/v1/get?isbn=978-4-7808-0204-7')
+      this.$axios.get('https://api.openbd.jp/v1/get?isbn=978-4-8156-0182-9')
         .then(function (response) {
-          self.dataUrl = response.data[0].summary.cover
-          console.log(self.dataUrl)
+          console.log({ response })
+          self.dataImg = response.data[0].summary.cover
+          self.dataTitle = response.data[0].summary.title
         })
+    },
+    deleteImg() {
+      this.dataImg = ''
     }
   }
 }
